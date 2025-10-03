2040e51623b2db09dcf0ff1964fd3b4068d8925b
Shivom76 <shivompaul76@gmail.com>
2025-10-03 21:44:49 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import "./css/Footer.css"
+
 export default function Footer(){
     return(
         <div className="bg-black text-white text-center w-auto bottom-0 p-4">
@@ -5,10 +7,10 @@ export default function Footer(){
             <p className="text-xs mb-4">Something written here</p>
             <hr className="w-2/3 mx-auto " />
             <div className="logos mt-3">
-                <a href="#" className="px-1"><i className="fa-brands fa-instagram"></i></a>
-                <a href="#" className="px-1"><i className="fa-brands fa-github"></i></a>
-                <a href="#" className="px-1"><i className="fa-brands fa-x-twitter"></i></a>
-                <a href="#" className="px-1"><i className="fa-brands fa-facebook"></i></a>  
+                <a href="#" className="px-1"><i className="fa-brands fa-instagram opacity-70"></i></a>
+                <a href="#" className="px-1"><i className="fa-brands fa-github opacity-70"></i></a>
+                <a href="#" className="px-1"><i className="fa-brands fa-x-twitter opacity-70"></i></a>
+                <a href="#" className="px-1"><i className="fa-brands fa-facebook opacity-70"></i></a>  
             </div>
         </div>
     )
